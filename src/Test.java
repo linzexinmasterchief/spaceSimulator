3006372462caf52e7b16eba2d2bc5d4c0cfe317a
lzx <1249574830@qq.com>
2017-03-24 20:52:55 +0800

    big button!

--- src/Test.java
+++ src/Test.java
@@ -1,15 +1,11 @@
 import javafx.application.Application;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
-import java.lang.annotation.Target;
 
 /**
  * Created by lzx on 2017/3/21.
@@ -26,13 +22,12 @@ public class Test extends Application{
     @Override
     public void start(Stage stage){
         stage = new Stage();
-        Stage stage2 = new Stage();
+        stage.setResizable(false);
 
         StackPane stackPane = new StackPane();
         scene = new Scene(stackPane,1025,561);
-        stage.setResizable(false);
 
-        scene2 = new Scene(new Group(), 300, 300);
+        scene2 = new Scene(new StackPane(), 300, 300);
 
         stage.setScene(scene);
         Button button = new Button("",new ImageView(new Image(getClass().getResourceAsStream("back.jpg"))));
@@ -41,11 +36,6 @@ public class Test extends Application{
             finalStage.setScene(scene2);
         });
         ((StackPane)scene.getRoot()).getChildren().add(button);
-//
-//        //一个带文本和图标的Label
-//        Image image = new Image(getClass().getResourceAsStream("back.jpg"));
-//        Label label3 = new Label("",new ImageView(image));
-//        ((Group) scene.getRoot()).getChildren().add(label3);
 
         stage.show();
     }
