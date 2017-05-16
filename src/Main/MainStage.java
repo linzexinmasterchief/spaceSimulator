ce0c6362dbfcfb02ea9a489cad1d149358f36aaa
lzx <1249574830@qq.com>
2017-05-16 21:45:31 +0800

    removed the title scene

--- src/Main/MainStage.java
+++ src/Main/MainStage.java
@@ -2,25 +2,16 @@ package Main;
 
 import javafx.application.Application;
 import javafx.scene.Group;
-import javafx.scene.image.Image;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import scenes.GameScene;
-import scenes.TitleScene;
-import sounds.Music;
 
 /**
  * Created by lzx on 2017/3/21.
  * this is the root class, start stage
  */
 
-public class MainStage extends Application implements Runnable {
+public class MainStage extends Application {
 
-    public static Group group;
-    public static StackPane stackPane;
-
-    public static Stage stage;
-    public static TitleScene titleScene;
     public static GameScene gameScene;
 
     public static void main(String[] args) {
@@ -28,40 +19,15 @@ public class MainStage extends Application implements Runnable {
     }
 
     @Override
-    public void start(Stage HAHAstage) {
-
-        group = new Group();
-        gameScene = new GameScene(group, 1000, 560);
-
-        stackPane = new StackPane();
-        titleScene = new TitleScene(stackPane, 1000, 560);
+    public void start(Stage stage) {
+        gameScene = new GameScene(new Group(), 1000, 560);
 
         stage = new Stage();
-        stage.setScene(titleScene);
+        stage.setScene(gameScene);
         stage.setResizable(false);
         stage.setTitle("SpaceSimulator");
-        stage.getIcons().add(new Image(getClass().getResourceAsStream("..\\pictures\\icon.png")));
         stage.setOnCloseRequest(event -> System.exit(0));
         stage.sizeToScene();
         stage.show();
-
-        Thread thread = new Thread(this);
-        thread.start();
-
-        Music music = new Music();
-        Thread thread2 = new Thread(music);
-        thread2.start();
-    }
-
-    @Override
-    public void run() {
-        while (true) {
-            try {
-                Thread.sleep(30);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            TitleScene.TitleThread();
-        }
     }
 }
