b488c28adcbac88eb0fa400ea8d5f9c73fdaa390
lzx <1249574830@qq.com>
2017-06-01 11:41:47 +0800

    add descriptions to codes

--- src/models/Universe.java
+++ src/models/Universe.java
@@ -5,50 +5,77 @@ import physics.GravityCalculate;
 
 /**
  * Created by lzx on 2017/5/26.
+ * this class is the core of the application,
+ * it is in charge of the position system
+ * and the gravity calculate (gravity calculate
+ * class is only a module)
  */
 public class Universe implements Runnable {
+
+    //define the size of the universe
     private double width;
     private double height;
 
+    //use a bufferStar for new input star
     private Star bufferStar;
+
+    //install the gravity module
     private GravityCalculate gravityCalculate;
+
+    //store all the stars in this universe
     private Star[] stars;
 
+    //some properties of the universe
     private boolean isExit;
     private boolean isPause;
     private boolean isNewStarExist;
 
+    //first constructor
     public Universe() {
+        //give a default universe size
         width = 0;
         height = 0;
 
+        //call the function to initialize all the stars in the universe
         initialize();
     }
 
+    //second constructor
     public Universe(double width, double height) {
+        //define the size of the universe based on the value input
         this.width = width;
         this.height = height;
 
+        //call the function to initialize all the stars in the universe
         initialize();
     }
 
+    //function used to initialize all the stars to their default value
     private void initialize() {
+        //initialize the gravity module object
         gravityCalculate = new GravityCalculate(stars);
 
+        //initialize the buffer star
         bufferStar = new Star();
 
+        //initialize universe properties
         isExit = false;
         isPause = false;
         isNewStarExist = false;
 
+        //initialize the star list of the universe
         stars = new Star[50];
 
+        //initialize every star in the star list
+        //this is very important and necessary, do not delete it
         for (int i = 0; i < stars.length; i++) {
             stars[i] = new Star();
         }
 
     }
 
+    //this is the function called on every physics cycle
+    //kind of like "fixed update" in unity
     private void GameThread() {
         if (isPause) {
             return;
@@ -71,19 +98,11 @@ public class Universe implements Runnable {
                     gravityCalculate.synchronize(stars);
                     gravityCalculate.gravityAcceleration(stars[F]);
                 });
-//            } else {
-//                stars[i].initialize();
-//                if (isNewStarExist) {
-//                    stars[i] = new Star(bufferStar);
-//                    bufferStar.remove();
-//
-//                    stars[i].onScreen = true;
-//                    isNewStarExist = false;
-//                }
             }
         }
     }
 
+    //a whole bunch of getter and setters
     public Star[] getStars() {
         return stars;
     }
@@ -116,6 +135,7 @@ public class Universe implements Runnable {
         this.height = height;
     }
 
+    //the main thread cycle of the universe
     @Override
     public void run() {
         while (!isExit) {
@@ -124,6 +144,7 @@ public class Universe implements Runnable {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            //call the specific used function
             GameThread();
         }
     }
