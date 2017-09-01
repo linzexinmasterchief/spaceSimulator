e3c99d4a616031445f47369aab4b311183807ba4
linzexinmasterchief <linzexinmasterchief@gmail.com>
2017-09-01 21:48:44 +0800

    base ui class

---
+++ src/models/uiComponentModels/UiBase.java
@@ -0,0 +1,46 @@
+package models.uiComponentModels;
+
+/**
+ * Created by lzx on 2017/9/1.
+ */
+public class UiBase implements UiComponent {
+
+    //x and y is the coordinate of the top left corner
+    double x = 0;
+
+    double y = 0;
+
+    double width = 0;
+
+    double height = 0;
+
+    @Override
+    public void setX(double value) {
+        x = value;
+    }
+
+    @Override
+    public void setY(double value) {
+        y = value;
+    }
+
+    @Override
+    public void setCenterX(double value) {
+        x = value - (width / 2);
+    }
+
+    @Override
+    public void setCenterY(double value) {
+        y = value - (height / 2);
+    }
+
+    @Override
+    public void setWidth(double value) {
+        width = value;
+    }
+
+    @Override
+    public void setHeight(double value) {
+        width = value;
+    }
+}
