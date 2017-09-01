099a5ca5db5e62356c623b3b9b2f11bd8ed7c5d1
linzexinmasterchief <linzexinmasterchief@gmail.com>
2017-09-01 21:46:08 +0800

    key setting for each function, still can't use

---
+++ src/Application/system/KeySetting.java
@@ -0,0 +1,27 @@
+package Application.system;
+
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+
+/**
+ * Created by lzx on 2017/9/1.
+ */
+public class KeySetting {
+
+    public static MouseButton selectStar = MouseButton.PRIMARY;
+
+    public static MouseButton newStar = MouseButton.SECONDARY;
+
+    public static KeyCode clearStar = KeyCode.C;
+
+    public static KeyCode togglePause = KeyCode.SPACE;
+
+    public static KeyCode moveCamForward = KeyCode.W;
+
+    public static KeyCode moveCamLeft = KeyCode.A;
+
+    public static KeyCode moveCamBackward = KeyCode.S;
+
+    public static KeyCode moveCamRight = KeyCode.D;
+
+}
